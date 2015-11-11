3089aa5ee443bb8eba71f718708b94a11126933a
Ben Rosen <benrosen78@gmail.com>
2015-11-11 00:50:51 -0600

    [music provider] make notifications actually work

--- api/providers/music/HBTSPlusMusicProvider.m
+++ api/providers/music/HBTSPlusMusicProvider.m
@@ -1,32 +1,34 @@
 #import "HBTSPlusMusicProvider.h"
 #import <Foundation/NSDistributedNotificationCenter.h>
+#import <MediaRemote/MediaRemote.h>
 
-extern CFStringRef kMRMediaRemoteNowPlayingInfoDidChangeNotification;
-extern CFStringRef kMRMediaRemoteNowPlayingInfoTitle;
-extern CFStringRef kMRMediaRemoteNowPlayingInfoArtist;
-
-@implementation HBTSPlusMusicProvider
+@implementation HBTSPlusMusicProvider {
+	NSString *_lastUsedSongName, *_lastUsedArtistName;
+}
 
 - (id)init {
 	if (self = [super init]) {
-		HBLogDebug(@"Yoooooooo  init caled.");
-
 		self.name = @"Music";
-		self.preferencesBundle = [NSBundle bundleWithPath:@"/Library/PreferenceBundles/TypeStatusPlus.bundle/"];
+		self.preferencesBundle = [NSBundle bundleWithPath:@"/Library/PreferenceBundles/TypeStatusPlusMusic.bundle/"];
 		self.preferencesClass = @"HBTSPlusMusicProviderListController";
 
-		[[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(typeStatusPlusMusic_mediaInfoDidChange:) name:(NSString *)kMRMediaRemoteNowPlayingInfoDidChangeNotification object:nil];
+		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(typeStatusPlusMusic_mediaInfoDidChange:) name:(NSString *)kMRMediaRemoteNowPlayingInfoDidChangeNotification object:nil];
 	}
 	return self;
 }
 
 - (void)typeStatusPlusMusic_mediaInfoDidChange:(NSNotification *)notification {
-	HBLogDebug(@"Yoooooooo caled.");
-	NSString *songName = notification.userInfo[(NSString *)kMRMediaRemoteNowPlayingInfoTitle];
-	NSString *artistName = notification.userInfo[(NSString *)kMRMediaRemoteNowPlayingInfoArtist];
-	if (songName && artistName) {
-		[self showNotification:[NSString stringWithFormat:@"The song %@ by %@ is now playing!", songName, artistName]];
-	}
+	MRMediaRemoteGetNowPlayingInfo(dispatch_get_main_queue(), ^(CFDictionaryRef result) {
+		NSDictionary *dictionary = (__bridge NSDictionary *)result;
+		NSString *songName = dictionary[(NSString *)kMRMediaRemoteNowPlayingInfoTitle];
+		NSString *artistName = dictionary[(NSString *)kMRMediaRemoteNowPlayingInfoArtist];
+		if (_lastUsedSongName && artistName && ![_lastUsedSongName isEqualToString:songName] && ![_lastUsedArtistName isEqualToString:artistName]) {
+			_lastUsedSongName = songName;
+			_lastUsedArtistName = artistName;
+			[self showNotification:[NSString stringWithFormat:@"The song %@ by %@ is now playing!", songName, artistName]];
+		}
+	});
+
 }
 
 @end
