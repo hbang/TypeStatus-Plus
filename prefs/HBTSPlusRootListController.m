fdaf456771246a4fa1f545d267a4913a1ba3eca7
Ben Rosen <benrosen78@gmail.com>
2015-11-14 16:00:07 -0600

    [prefs] implement BigBeautifulBanner™

--- prefs/HBTSPlusRootListController.m
+++ prefs/HBTSPlusRootListController.m
@@ -19,13 +19,13 @@
 	return @"Root";
 }
 
-+ (UIColor *)hb_tintColor {
+/*+ (UIColor *)hb_tintColor {
 	return [UIColor colorWithRed:0.345f green:0.337f blue:0.839f alpha:1.00f];
 }
 
 + (BOOL)hb_invertedNavigationBar {
 	return YES;
-}
+}*/
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
