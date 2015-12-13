b9c7afd383893569673559d9c5463cd1e3e6e707
Ben Rosen <benrosen78@gmail.com>
2015-12-12 21:32:57 -0600

    [headers] fix issue with HBTSStatusBarAlertServer header

---
+++ typestatus-private/HBTSStatusBarAlertServer.h
@@ -0,0 +1,9 @@
+@class UIStatusBar;
+
+@interface HBTSStatusBarAlertServer : NSObject
+
++ (void)sendAlertWithIconName:(NSString *)iconName title:(NSString *)title content:(NSString *)content;
+
++ (void)hide;
+
+@end
