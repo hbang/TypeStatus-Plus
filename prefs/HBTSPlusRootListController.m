60689c171a069330e8a25566f0d4232e9cb4430d
Ben Rosen <benrosen78@gmail.com>
2015-12-15 20:58:19 -0600

    [prefs] redo the root list controller using a dark mode

--- prefs/HBTSPlusRootListController.m
+++ prefs/HBTSPlusRootListController.m
@@ -2,6 +2,8 @@
 #import <TypeStatusPlusProvider/HBTSPlusProviderController.h>
 #import <TypeStatusPlusProvider/HBTSPlusProvider.h>
 #import <Preferences/PSSpecifier.h>
+#import <UIKit/UIImage+Private.h>
+#import <CepheiPrefs/HBSupportController.h>
 
 @implementation HBTSPlusRootListController {
 	NSArray *_providers;
@@ -20,23 +22,64 @@
 }
 
 + (UIColor *)hb_tintColor {
-	return [UIColor colorWithRed:0.212f green:0.635f blue:0.835f alpha:1.00f];
+	return [UIColor colorWithRed:0.094 green:0.412 blue:0.325 alpha:1.00];
+}
+
++ (UIColor *)hb_navigationBarTintColor {
+	return [UIColor colorWithRed:0.059 green:0.059 blue:0.059 alpha:1.00];
+}
+
++ (BOOL)hb_invertedNavigationBar {
+	return YES;
+}
+
++ (BOOL)hb_translucentNavigationBar {
+	return YES;
+}
+
++ (UIColor *)hb_tableViewCellTextColor {
+	return [UIColor whiteColor];
+}
+
++ (UIColor *)hb_tableViewCellBackgroundColor {
+	return [UIColor colorWithRed:0.055 green:0.055 blue:0.055 alpha:1.00];
+}
+
++ (UIColor *)hb_tableViewCellSeparatorColor {
+	return [UIColor colorWithRed:0.047 green:0.047 blue:0.047 alpha:1.00];
+}
+
++ (UIColor *)hb_tableViewBackgroundColor {
+	return [UIColor colorWithRed:0.075 green:0.075 blue:0.075 alpha:1.00];
 }
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
+
 	[self _updateHandlers];
+
+	UIImage *headerLogo = [UIImage imageNamed:@"headerlogo" inBundle:[NSBundle bundleForClass:self.class]];
+	self.navigationItem.titleView = [[[UIImageView alloc] initWithImage:headerLogo] autorelease];
+	self.navigationItem.titleView.alpha = 0.0;
+
+	[self performSelector:@selector(animateIconAlpha) withObject:nil afterDelay:0.5];
 }
 
-- (void)reloadSpecifiers {
-	[super reloadSpecifiers];
-	//[self _updateHandlers];
+- (void)animateIconAlpha {
+	[UIView animateWithDuration:0.5 animations:^{
+		self.navigationItem.titleView.alpha = 1;
+	} completion:nil];
+}
+
+- (void)showSupportEmailController {
+	UIViewController *viewController = (UIViewController *)[HBSupportController supportViewControllerForBundle:[NSBundle bundleForClass:self.class] preferencesIdentifier:@"com.tweakbattles.chrysalis"];
+	[self.navigationController pushViewController:viewController animated:YES];
 }
 
 #pragma mark - Update state
 
 - (void)_updateHandlers {
-	HBTSPlusProviderController *providerController = [HBTSPlusProviderController sharedInstance];
+	/*HBTSPlusProviderController *providerController = [HBTSPlusProviderController sharedInstance];
 	[providerController loadProviders];
 
 	_providers = [providerController.providers copy];
@@ -78,7 +121,7 @@
 		[self removeSpecifierID:@"ProvidersGroupCell"];
 	}
 
-	HBLogDebug(@"This is a log to test out if something is run or if this is where it crashes ");
+	HBLogDebug(@"This is a log to test out if something is run or if this is where it crashes ");*/
 }
 
 #pragma mark - Memory management
