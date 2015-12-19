f126597ca476e72e914846d398b70cfc6da40a47
Ben Rosen <benrosen78@gmail.com>
2015-12-18 21:48:36 -0600

    [prefs] this shouldn't be here anymore

--- prefs/HBTSPlusRootListController.m
+++ prefs/HBTSPlusRootListController.m
@@ -2,9 +2,7 @@
 #import <UIKit/UIImage+Private.h>
 #import <CepheiPrefs/HBSupportController.h>
 
-@implementation HBTSPlusRootListController {
-	NSArray *_providers;
-}
+@implementation HBTSPlusRootListController
 
 + (NSString *)hb_shareText {
 	return @"Couldn't be more happy I purchased TypeStatus+, the most advanced way to see who is typing. Available at BigBoss today for only 99 cents!";
@@ -53,8 +51,6 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 
-	[self _updateHandlers];
-
 	UIImage *headerLogo = [UIImage imageNamed:@"headerlogo" inBundle:[NSBundle bundleForClass:self.class]];
 	self.navigationItem.titleView = [[[UIImageView alloc] initWithImage:headerLogo] autorelease];
 	self.navigationItem.titleView.alpha = 0.0;
@@ -71,62 +67,6 @@
 - (void)showSupportEmailController {
 	UIViewController *viewController = (UIViewController *)[HBSupportController supportViewControllerForBundle:[NSBundle bundleForClass:self.class] preferencesIdentifier:@"com.tweakbattles.chrysalis"];
 	[self.navigationController pushViewController:viewController animated:YES];
-}
-
-#pragma mark - Update state
-
-- (void)_updateHandlers {
-	/*HBTSPlusProviderController *providerController = [HBTSPlusProviderController sharedInstance];
-	[providerController loadProviders];
-
-	_providers = [providerController.providers copy];
-
-	HBLogDebug(@"providers array is %@", _providers);
-
-	NSMutableArray *newSpecifiers = [NSMutableArray array];
-
-	for (HBTSPlusProvider *provider in _providers) {
-		PSSpecifier *specifier = [PSSpecifier preferenceSpecifierNamed:provider.name target:self set:@selector(setPreferenceValue:specifier:) get:@selector(readPreferenceValue:) detail:Nil cell:PSLinkCell edit:Nil];
-
-		HBLogDebug(@"The preference bundle is %@, preference class is %@", provider.preferencesBundle, provider.preferencesClass);
-
-		if (!provider.preferencesBundle ||  !provider.preferencesClass || !provider.appIdentifier) {
-			HBLogError(@"Necessary details not provided for %@", provider.name);
-			continue;
-		}
-
-		specifier.properties = [@{
-			PSIDKey: provider.appIdentifier,
-			PSBundleIsControllerKey: @YES,
-			PSLazilyLoadedBundleKey: provider.preferencesBundle.bundlePath,
-			PSDetailControllerClassKey: provider.preferencesClass,
-			PSLazyIconAppID: provider.appIdentifier,
-			PSLazyIconLoading: @YES
-		} mutableCopy];
-
-		specifier.controllerLoadAction = @selector(lazyLoadBundle:);
-
-		[newSpecifiers addObject:specifier];
-	}
-
-	HBLogInfo(@"The specifiers loaded into preference are: %@", newSpecifiers);
-
-	if (newSpecifiers.count > 0) {
-		[self removeSpecifierID:@"ProvidersNoneInstalledGroupCell"];
-		[self insertContiguousSpecifiers:newSpecifiers afterSpecifierID:@"ProvidersGroupCell" animated:YES];
-	} else {
-		[self removeSpecifierID:@"ProvidersGroupCell"];
-	}
-
-	HBLogDebug(@"This is a log to test out if something is run or if this is where it crashes ");*/
-}
-
-#pragma mark - Memory management
-
-- (void)dealloc {
-	[_providers release];
-
-	[super dealloc];
 }
 
 @end
