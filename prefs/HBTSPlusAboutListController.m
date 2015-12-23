d758b8d62aaf0725679f653b72b87ed16bcc13f2
Ben Rosen <benrosen78@gmail.com>
2015-12-23 10:12:53 -0600

    [prefs] fully impl the about list controller

--- prefs/HBTSPlusAboutListController.m
+++ prefs/HBTSPlusAboutListController.m
@@ -6,12 +6,4 @@
 	return @"About";
 }
 
-+ (UIColor *)hb_tintColor {
-	return [UIColor colorWithRed:0.235f green:0.671f blue:0.855f alpha:1.00f];
-}
-
-+ (BOOL)hb_invertedNavigationBar {
-	return YES;
-}
-
 @end
