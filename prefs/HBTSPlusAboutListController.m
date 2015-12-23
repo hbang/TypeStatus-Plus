1cce2a0dbb04d45564d90dcb5cb60be7ad7d6fae
Ben Rosen <benrosen78@gmail.com>
2015-12-23 10:44:18 -0600

    [prefs] add in a nice footer to the about controller w tech support

--- prefs/HBTSPlusAboutListController.m
+++ prefs/HBTSPlusAboutListController.m
@@ -1,9 +1,21 @@
 #import "HBTSPlusAboutListController.h"
+#import <Preferences/PSSpecifier.h>
+#import <TechSupport/TechSupport.h>
 
 @implementation HBTSPlusAboutListController
 
 + (NSString *)hb_specifierPlist {
 	return @"About";
+}
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+
+	TSPackage *package = [[TSPackage alloc] initWithIdentifier:@"ws.hbang.typestatusplus"];
+
+	PSSpecifier *specifier = [self.specifiers lastObject];
+	[specifier setProperty:[NSString stringWithFormat:@"%@\n%@", package.name, [NSString stringWithFormat:@"Version: %@", package.version]] forKey:PSFooterTextGroupKey];
+
 }
 
 @end
