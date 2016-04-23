ac36d1195b87bd3b40bae90c03197da5e42d986e
Adam Demasi <adam@hbang.com.au>
2016-04-23 23:59:53 +0930

    [prefs] use new appearance api

--- prefs/HBTSPlusRootListController.m
+++ prefs/HBTSPlusRootListController.m
@@ -1,8 +1,11 @@
 #include "HBTSPlusRootListController.h"
+#import <CepheiPrefs/HBAppearanceSettings.h>
 #import <CepheiPrefs/HBSupportController.h>
 #import <UIKit/UIImage+Private.h>
 
 @implementation HBTSPlusRootListController
+
+#pragma mark - HBListController
 
 + (NSString *)hb_shareText {
 	return @"I couldn’t be more happy I joined the exclusive TypeStatus Plus beta group. You can join, too!";
@@ -16,44 +19,22 @@
 	return @"Root";
 }
 
-+ (UIColor *)hb_tintColor {
-	return [UIColor colorWithRed:0.094 green:0.412 blue:0.325 alpha:1.00];
-}
-
-+ (UIColor *)hb_navigationBarTintColor {
-	return [UIColor colorWithRed:0.055 green:0.055 blue:0.055 alpha:1.00];
-}
-
-+ (BOOL)hb_invertedNavigationBar {
-	return YES;
-}
-
-+ (BOOL)hb_translucentNavigationBar {
-	return NO;
-}
-
-+ (UIColor *)hb_tableViewCellTextColor {
-	return [UIColor whiteColor];
-}
-
-+ (UIColor *)hb_tableViewCellBackgroundColor {
-	return [UIColor colorWithRed:0.055 green:0.055 blue:0.055 alpha:1.00];
-}
-
-+ (UIColor *)hb_tableViewCellSeparatorColor {
-	return [UIColor colorWithRed:0.120 green:0.120 blue:0.120 alpha:1.00];
-}
-
-+ (UIColor *)hb_tableViewCellSelectionColor {
-	return [UIColor colorWithRed:0.149 green:0.149 blue:0.149 alpha:1.00];
-}
-
-+ (UIColor *)hb_tableViewBackgroundColor {
-	return [UIColor colorWithRed:0.089 green:0.089 blue:0.089 alpha:1.00];
-}
+#pragma mark - PSListController
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
+
+	HBAppearanceSettings *appearance = [[HBAppearanceSettings alloc] init];
+	appearance.tintColor = [UIColor colorWithRed:0.094 green:0.412 blue:0.325 alpha:1.00];
+	appearance.navigationBarTintColor = [UIColor colorWithRed:0.055 green:0.055 blue:0.055 alpha:1.00];
+	appearance.invertedNavigationBar = YES;
+	appearance.translucentNavigationBar = NO;
+	appearance.tableViewCellTextColor = [UIColor whiteColor];
+	appearance.tableViewCellBackgroundColor = [UIColor colorWithRed:0.055 green:0.055 blue:0.055 alpha:1.00];
+	appearance.tableViewCellSeparatorColor = [UIColor colorWithRed:0.120 green:0.120 blue:0.120 alpha:1.00];
+	appearance.tableViewCellSelectionColor = [UIColor colorWithRed:0.149 green:0.149 blue:0.149 alpha:1.00];
+	appearance.tableViewBackgroundColor = [UIColor colorWithRed:0.089 green:0.089 blue:0.089 alpha:1.00];
+	self.hb_appearanceSettings = appearance;
 
 	UIImage *headerLogo = [UIImage imageNamed:@"headerlogo" inBundle:[NSBundle bundleForClass:self.class]];
 	self.navigationItem.titleView = [[[UIImageView alloc] initWithImage:headerLogo] autorelease];
@@ -61,6 +42,8 @@
 
 	[self performSelector:@selector(animateIconAlpha) withObject:nil afterDelay:0.5];
 }
+
+#pragma mark - Callbacks
 
 - (void)animateIconAlpha {
 	[UIView animateWithDuration:0.5 animations:^{
