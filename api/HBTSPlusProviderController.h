94bcbbc75d0eaa4d23164ac2d038c5dd116245a6
Adam Demasi <adam@hbang.com.au>
2016-10-01 10:57:44 +0930

    [HBTSPlusProviderController] yucky reminders for me to document stuff

--- api/HBTSPlusProviderController.h
+++ api/HBTSPlusProviderController.h
@@ -1,17 +1,47 @@
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ * TODO: Document
+ */
 static NSString *const kHBTSApplicationBundleIdentifierKey = @"HBTSApplicationBundleIdentifier";
+
+/**
+ * TODO: Document
+ */
 static NSString *const kHBTSKeepApplicationAliveKey = @"HBTSKeepApplicationAlive";
 
 @class HBTSPlusProvider;
 
+/**
+ * TODO: Document
+ */
 @interface HBTSPlusProviderController : NSObject
 
+/**
+ * TODO: Document
+ */
 + (instancetype)sharedInstance;
 
+/**
+ * TODO: Document
+ */
 @property (nonatomic, retain, readonly) NSMutableArray *providers;
 
+/**
+ * TODO: Document
+ */
 - (HBTSPlusProvider *)providerWithAppIdentifier:(NSString *)appIdentifier;
+
+/**
+ * TODO: Document
+ */
 - (BOOL)providerIsEnabled:(HBTSPlusProvider *)provider;
 
+/**
+ * TODO: Document
+ */
 - (BOOL)applicationWithIdentifierRequiresBackgrounding:(NSString *)identifier;
 
 @end
+
+NS_ASSUME_NONNULL_END
