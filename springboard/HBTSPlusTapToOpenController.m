94b3ddd6ddb3aaa773daa4e7ce1c484b39d74d03
Adam Demasi <adam@hbang.com.au>
2016-11-06 21:56:02 +1030

    [springboard] use SBS open url function so we get a back button

--- springboard/HBTSPlusTapToOpenController.m
+++ springboard/HBTSPlusTapToOpenController.m
@@ -1,6 +1,9 @@
 #import "HBTSPlusTapToOpenController.h"
 #import <Foundation/NSDistributedNotificationCenter.h>
+#import <MobileCoreServices/LSApplicationProxy.h>
+#import <MobileCoreServices/LSApplicationWorkspace.h>
 #import <SpringBoard/SpringBoard.h>
+#import <SpringBoardServices/SpringBoardServices.h>
 
 @implementation HBTSPlusTapToOpenController {
 	NSString *_currentSender;
@@ -42,20 +45,37 @@
 		// if this is a messages notification
 		if ([_appIdentifier isEqualToString:@"com.apple.MobileSMS"]) {
 			// open the url ourselves
-			[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"sms://open?address=%@", _currentSender]]];
+			[self _openURL:[NSURL URLWithString:[NSString stringWithFormat:@"sms://open?address=%@", _currentSender]] bundleIdentifier:@"com.apple.MobileSMS"];
 
 			// we don’t need this any more
 			_currentSender = nil;
 		} else if (_actionURL) {
 			// if we got a url, open that
-			[[UIApplication sharedApplication] openURL:_actionURL];
+			[self _openURL:_actionURL bundleIdentifier:nil];
 		} else {
 			// or fall back to just opening the app like normal
-			[(SpringBoard *)[UIApplication sharedApplication] launchApplicationWithIdentifier:_appIdentifier suspended:NO];
+			[self _openURL:nil bundleIdentifier:_appIdentifier];
 		}
 	});
 
 	return nil;
+}
+
+- (void)_openURL:(NSURL *)url bundleIdentifier:(NSString *)bundleIdentifier {
+	if (!bundleIdentifier) {
+		NSArray <LSApplicationProxy *> *apps = [[LSApplicationWorkspace defaultWorkspace] applicationsAvailableForHandlingURLScheme:url.scheme];
+
+		if (apps.count == 0) {
+			HBLogError(@"huh? no app available to open %@", url);
+			return;
+		}
+
+		bundleIdentifier = apps[0].applicationIdentifier;
+	}
+
+	SBSLaunchApplicationWithIdentifierAndURLAndLaunchOptions(bundleIdentifier, url, @{}, @{
+		SBSApplicationLaunchOptionUnlockDeviceKey: @YES
+	}, NO);
 }
 
 @end
