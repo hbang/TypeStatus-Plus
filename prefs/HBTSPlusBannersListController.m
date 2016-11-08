e315c867d1e3eaa617140d3df6c3188a7b4864b0
Adam Demasi <adam@hbang.com.au>
2016-11-09 00:20:41 +1030

    [prefs] add test button

--- prefs/HBTSPlusBannersListController.m
+++ prefs/HBTSPlusBannersListController.m
@@ -1,9 +1,16 @@
 #import "HBTSPlusBannersListController.h"
+#include <notify.h>
 
 @implementation HBTSPlusBannersListController
 
 + (NSString *)hb_specifierPlist {
 	return @"Banners";
+}
+
+#pragma mark - Callbacks
+
+- (void)testAlert {
+	notify_post("ws.hbang.typestatus/TestTyping");
 }
 
 @end
