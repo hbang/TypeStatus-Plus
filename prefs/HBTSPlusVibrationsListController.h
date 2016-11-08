4d76064dc9cb119a0c81075e6aa5ad1db07d4b7e
Adam Demasi <adam@hbang.com.au>
2016-11-09 01:30:42 +1030

    [prefs] merge vibrations section into alerts

--- prefs/HBTSPlusVibrationsListController.h
+++
@@ -1,5 +0,0 @@
-#import <CepheiPrefs/HBListController.h>
-
-@interface HBTSPlusVibrationsListController : HBListController
-
-@end
--- prefs/HBTSPlusVibrationsListController.m
+++
@@ -1,9 +0,0 @@
-#import "HBTSPlusVibrationsListController.h"
-
-@implementation HBTSPlusVibrationsListController
-
-+ (NSString *)hb_specifierPlist {
-	return @"Vibrations";
-}
-
-@end
